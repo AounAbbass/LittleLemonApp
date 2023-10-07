33aa16942bd9337f56ef44aad16fbea1f712da51
Aoun Abbass <maliccaoun@gmail.com>
2023-10-07 13:05:29 +0500

    second commit

--- FirstProject/App.js
+++ FirstProject/App.js
@@ -3,12 +3,13 @@ import { View, StyleSheet } from 'react-native';
 
  // Import a local component here
  import LittleLemonHeader from './components/LittleLemonHeader';
-
+ import WelcomeScreen from './WelcomeScreen';
  export default function App() {
  return (
  <View
  style={styles.container}>
  <LittleLemonHeader />
+ <WelcomeScreen />
  </View>
  );
  }
