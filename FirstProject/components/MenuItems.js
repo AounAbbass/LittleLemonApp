294722a3f06cdb4815d0cc301d5316a2389bac60
Aoun Abbass <maliccaoun@gmail.com>
2023-10-09 12:36:02 +0500

    seventh commit

--- FirstProject/components/MenuItems.js
+++ FirstProject/components/MenuItems.js
@@ -1,16 +1,31 @@
-import { View, Text, StyleSheet, FlatList } from 'react-native';
+import { View, Text, StyleSheet, SectionList } from 'react-native';
 
 const menuItemsToDisplay = [
+  {
+    title: 'Appetizers',
+    data:[
     { name: 'Hummus', price: '$5.00', id: '1A' },
     { name: 'Moutabal', price: '$5.00', id: '2B' },
     { name: 'Falafel', price: '$7.50', id: '3C' },
     { name: 'Marinated Olives', price: '$5.00', id: '4D' },
     { name: 'Kofta', price: '$5.00', id: '5E' },
     { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+    ],
+  },
+  
+  {
+    title: 'Main Dishes',
+    data:[
     { name: 'Lentil Burger', price: '$10.00', id: '7G' },
     { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
     { name: 'Kofta Burger', price: '$11.00', id: '9I' },
     { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+    ],
+  },
+
+  {
+    title: 'Sides',
+    data:[
     { name: 'Fries', price: '$3.00', id: '11K' },
     { name: 'Buttered Rice', price: '$3.00', id: '12L' },
     { name: 'Bread Sticks', price: '$3.00', id: '13M' },
@@ -18,13 +33,20 @@ const menuItemsToDisplay = [
     { name: 'Lentil Soup', price: '$3.75', id: '15O' },
     { name: 'Greek Salad', price: '$6.00', id: '16Q' },
     { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+    ],
+  },
+
+  {
+    title: 'Desserts',
+    data:[
     { name: 'Baklava', price: '$3.00', id: '18S' },
     { name: 'Tartufo', price: '$3.00', id: '19T' },
     { name: 'Tiramisu', price: '$5.00', id: '20U' },
     { name: 'Panna Cotta', price: '$5.00', id: '21V' },
-  ];
+    ],
+  },
+];
 
-  const Header = () => <Text style={menuStyles.headerText}>View Menu</Text>;
 
   const Item = ({ name, price }) => (
     <View style={menuStyles.innerContainer}>
@@ -33,42 +55,54 @@ const menuItemsToDisplay = [
     </View>
   );
   
+  const Separator = () => <View style={menuStyles.separator} />;
+
+
   const MenuItems = () => {
     const renderItem = ({ item }) => <Item name={item.name} price={item.price} />;
   
+    const renderSectionHeader = ({ section: { title } }) => (
+      <Text style={menuStyles.sectionHeader}>{title} </Text>
+    );
+    
     return (
       <View style={menuStyles.container}>
-        <FlatList
-          data={menuItemsToDisplay}
+        <SectionList
           keyExtractor={(item) => item.id}
+          sections={menuItemsToDisplay}
           renderItem={renderItem}
-          ListHeaderComponent={Header}
-          >
-          </FlatList>
+          renderSectionHeader={renderSectionHeader}
+          ItemSeparatorComponent={Separator}>
+        </SectionList>
       </View>
     );
   };
   
   const menuStyles = StyleSheet.create({
     container: {
-      flex: 1,
+      flex:10.95,
     },
     innerContainer: {
       paddingHorizontal: 40,
       paddingVertical: 20,
-      flexDirection: 'row',
-      justifyContent: 'space-between'
+      backgroundColor: '#333333',
     },
-    headerText: {
-      fontSize: 40,
-      color: 'white',
-      justifyContent: 'center',
-      paddingLeft: 120,
+    sectionHeader: {
+      backgroundColor: '#F4CE14',
+      color: '#333333',
+      fontSize: 34,
+      flexWrap: 'wrap',
+      textAlign: 'center',
     },
     itemText: {
       color: '#F4CE14',
-      fontSize: 20,
+      fontSize: 32,
     },
+    separator: {
+      borderBottomWidth: 1,
+      borderColor: '#EDEFEE',
+    },
+
   });
   
   export default MenuItems;
